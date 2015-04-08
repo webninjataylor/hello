8103a9bff8b4f0c6fa5a97d9499170a9f93ab1ea
webninjataylor <webninjataylor@gmail.com>
2015-04-08 19:09:21 -0400

    Hello uses Greeter.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,8 @@
+require 'greeter'
+
 # Default is "World"
 # Author: Taylor Johnson (webninjataylor@gmail.com)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
